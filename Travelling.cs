07ca2f8e2aea4ff9eb30b317336192ec8022af50
Inesa Kostova <kostovainesa@gmail.com>
2025-03-10 12:24:49 +0200

    Add files via upload

---
+++ Travelling.cs
@@ -0,0 +1,11 @@
+﻿string destination = Console.ReadLine();
+while(destination!="End")
+{
+    double budget = double.Parse(Console.ReadLine());
+    while(budget>0)
+    {
+       budget-=double.Parse(Console.ReadLine());
+    }
+    Console.WriteLine($"Going to {destination}!");
+    destination = Console.ReadLine();
+}
