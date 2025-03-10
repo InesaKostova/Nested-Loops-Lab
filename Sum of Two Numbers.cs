26b84bb39e9ca93ad2bb7d17eb0988db3e406aaa
Inesa Kostova <kostovainesa@gmail.com>
2025-03-10 11:41:14 +0200

    Add files via upload

---
+++ Sum of Two Numbers.cs
@@ -0,0 +1,18 @@
+﻿int start = int.Parse(Console.ReadLine());
+int finish=int.Parse(Console.ReadLine());
+int magicNum=int.Parse(Console.ReadLine());
+
+int combinations = 0;
+for(int x1=start; x1<=finish; x1++)
+{
+    for(int x2=start; x2<=finish; x2++)
+    { combinations++;
+        if(x1+x2==magicNum)
+        {
+            Console.WriteLine($"Combination N:{combinations} ({x1} + {x2} = {magicNum})");
+            return;
+        }
+
+    }
+}
+Console.WriteLine($"{combinations} combinations - neither equals {magicNum}");
